5f81972832c01c142e761f06912d96fec99c5b46
Dmitriy_Kuprin <d.i.kuprin@gmail.com>
2020-09-19 13:09:28 +0300

    R0.1 firebase auth

--- client/src/components/Auth/AuthForm/index.js
+++ client/src/components/Auth/AuthForm/index.js
@@ -2,64 +2,87 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import { Input, Button, Layout, Text } from "@ui-kitten/components";
 // import auth from '@react-native-firebase/auth';
-import * as firebase from "../../../../firebase";
-// import  "@firebase/firestore";
-// import "@firebase/auth";
+import { firebase } from "../../../../firebase";
+import "@firebase/firestore";
+import "@firebase/auth";
 
-// const AuthForm = () => {
-//   const [login, setLogin] = React.useState('');
-//   const [pass, setPass] = React.useState('');
-//   function Submit() {}
-//   return (
-//     <Layout style={styles.container} level="1">
-//       <Input
-//         placeholder="Login"
-//         value={login}
-//         onChangeText={(nextValue) => setLogin(nextValue)}
-//       />
-//       <Input
-//         secureTextEntry={true}
-//         placeholder="Password"
-//         value={pass}
-//         onChangeText={(nextValue) => setPass(nextValue)}
-//       />
-//       <Button onPress={Submit}>Register</Button>
-//     </Layout>
-//   );
-// };
 
-function AuthForm() {
-  // Set an initializing state whilst Firebase connects
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
+const AuthForm = () => {
+  const [email, setLogin] = React.useState('');
+  const [pass, setPass] = React.useState('');
+  async function Submit() {
+    let q = await firebase.auth().signInWithEmailAndPassword(email, pass);
+    // .signInWithEmailAndPassword(email, pass)
+    // .then(
+    //   this.onLoginSuccess.bind(this),
 
-  // Handle user state changes
-  function onAuthStateChanged(user) {
-    setUser(user);
-    if (initializing) setInitializing(false);
+    //   )
+    //   .catch(error => {
+    //     let errorCode = error.code;
+    //     let errorMessage = error.message;
+    //     if (errorCode == 'auth / weak-password') {
+    //       this.onLoginFailure.bind(this)('Weak Password!');
+    //     } else {
+    //       this.onLoginFailure.bind(this)(errorMessage);
+    //     }
+    //   });
+    console.log('>>>>>>>>>>>>', q)
   }
 
   useEffect(() => {
-    const subscriber = firebase.firebase.auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
-  }, []);
 
-  if (initializing) return null;
-
-  if (!user) {
-    return (
-      <View>
-        <Text>Login</Text>
-      </View>
-    );
-  }
-
+  })
   return (
-    <View>
-      <Text>Welcome {user.email}</Text>
-    </View>
+    <Layout style={styles.container} level="1">
+      <Input
+        placeholder="Email"
+        value={email}
+        onChangeText={(nextValue) => setLogin(nextValue)}
+      />
+      <Input
+        secureTextEntry={true}
+        placeholder="Password"
+        value={pass}
+        onChangeText={(nextValue) => setPass(nextValue)}
+      />
+      <Button onPress={Submit}>Register</Button>
+
+    </Layout>
   );
-}
+};
+
+// function AuthForm() {
+//   // Set an initializing state whilst Firebase connects
+//   const [initializing, setInitializing] = useState(true);
+//   const [user, setUser] = useState();
+
+//   // Handle user state changes
+//   function onAuthStateChanged(user) {
+//     setUser(user);
+//     if (initializing) setInitializing(false);
+//   }
+
+//   useEffect(() => {
+//     const subscriber = firebase.firebase.auth().onAuthStateChanged(onAuthStateChanged);
+//     return subscriber; // unsubscribe on unmount
+//   }, []);
+
+//   if (initializing) return null;
+
+//   if (!user) {
+//     return (
+//       <View>
+//         <Text>Login</Text>
+//       </View>
+//     );
+//   }
+
+//   return (
+//     <View>
+//       <Text>Welcome {user.email}</Text>
+//     </View>
+//   );
+// }
 
 styles = {};
 export default AuthForm;
