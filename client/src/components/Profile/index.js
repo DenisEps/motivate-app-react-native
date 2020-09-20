c61362cb09b5e442677402449c2a878df8d2976e
LamNik324 <89999809989@yandex.ru>
2020-09-20 18:25:20 +0300

    test notifications - 2.0

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -4,19 +4,9 @@ function Profile() {
   useEffect(() => {
     //Login, Email, phone, avatar
   }, []);
-  return (
-    <>
-      <p>Edit Profile</p>
-      <img src=""></img>
-      <button>Photo</button>
-      <p>Login</p>
-      <input value="#"></input>
-      <p>Email Address</p>
-      <input value="#"></input>
-      <p>Phone</p>
-      <input value="#"></input>
-    </>
-  )
+  // return (
+
+  // )
 }
 
 export default Profile;
