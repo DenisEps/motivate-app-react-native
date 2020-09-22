1985ba1077b068430bee1b8dcdd50ba0f9fe42ea
LamNik324 <89999809989@yandex.ru>
2020-09-22 11:04:19 +0300

    fixed auth trought google

--- client/src/components/Auth/RegistrationForm/index.js
+++ client/src/components/Auth/RegistrationForm/index.js
@@ -37,11 +37,11 @@ const RegistrationForm = () => {
             .collection("users")
             .doc(info.user.uid)
             .set({
-              email: info.user.email,
-              displayName: info.user.displayName,
-              photoURL: info.user.photoURL,
-              phoneNumber: info.user.phoneNumber,
-              emailVerified: info.user.emailVerified,
+              email: info.user.email == null ? '' : info.user.email,
+              displayName: info.user.displayName == null ? '' : info.user.displayName,
+              photoURL: info.user.photoURL == null ? '' : info.user.photoURL,
+              phoneNumber: info.user.phoneNumber == null ? '' : info.user.phoneNumber,
+              emailVerified: info.user.emailVerified == null ? '' : info.user.emailVerified,
               habits: [],
               level: 1,
             });
--- client/src/navigation/RootNavigator/index.js
+++ client/src/navigation/RootNavigator/index.js
@@ -37,7 +37,7 @@ const load = async () => {
     setError(err.message)
   }
 }
-  return auth ? <TabNavigator /> : <StartForm />;
+  return auth ? <StartForm /> : <StartForm />;
 };
 
 export default RootNavigator;
